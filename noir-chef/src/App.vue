3b64824015b3722c8d51cb4227702a30a1c9142c
Favour Ukonu <favouroukonu@gmail.com>
2024-01-15 18:50:25 +0100

    Feat: Footer component added

--- noir-chef/src/App.vue
+++ noir-chef/src/App.vue
@@ -4,6 +4,7 @@ import Hero from "./components/Hero.vue";
 import Menu from "./components/Menu.vue";
 import Specialities from "./components/Specialities.vue";
 import OurStory from "./components/OurStory.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import OurStory from "./components/OurStory.vue";
     <Specialities />
     <Dining />
     <OurStory />
+    <Footer />
     <!-- <Hero /> -->
   </div>
 </template>
---
+++ noir-chef/src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-foundation">
+    <div></div>
+    <h1 class="text-textColor">Footer</h1>
+  </div>
+</template>
+
+<script>
+// export default {
+//   name: "Footer",
+// };
+</script>
+
+<style></style>
