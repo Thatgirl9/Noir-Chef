28120b1345e96927b4b1626409fdc74eae36adad
Favour Ukonu <favouroukonu@gmail.com>
2024-01-15 18:22:52 +0100

    Chore: Our Story component completed

--- noir-chef/src/components/OurStory.vue
+++ noir-chef/src/components/OurStory.vue
@@ -1,6 +1,48 @@
 <template>
   <div class="bg-foundation">
-    <h1 class="text-textColor">Our Story</h1>
+    <div
+      class="pt-[9em] text-center flex flex-col justify-center items-center gap-[2em] mb-[8em]"
+    >
+      <h1 class="text-headerText font-header text-4xl font-semibold">
+        Our Story
+      </h1>
+      <p
+        class="text-textColor font-bigText font-normal text-2xl sm:text-2xl w-[90%] sm:max-w-none"
+      >
+        A journey for making successful luxury restaurant<br
+          class="hidden sm:block"
+        />
+        with the best services
+      </p>
+    </div>
+
+    <div class="flex justify-center pb-[8em]">
+      <div
+        class="flex gap-8 lg:gap-[7em] flex-col lg:flex-row w-[80%] sm:w-[normal] justify-center items-center lg:items-end"
+      >
+        <div>
+          <img src="../assets/Rectangle 23.png" class="sm:h-[30em] h-[19em]" />
+        </div>
+        <div
+          class="text-textColor flex gap-[0.8em] flex-col items-center justify-center"
+        >
+          <p class="w-[29ch] md:w-[90%] lg:w-[29ch]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            feugiat urna id leo euismod rhoncus. Aliquam erat volutpat. Nulla id
+            aliquam neque, at dignissim quam. Praesent et lacus accumsan,
+            consequat nisl a, mattis sapien.
+          </p>
+          <p class="w-[29ch] md:w-[90%] lg:w-[29ch]">
+            Nam sodales ullamcorper aliquet. Phasellus ut pretium libero, vitae
+            imperdiet purus. Sed sed tincidunt velit. Aliquam vitae ipsum
+            molestie, vehicula nisi quis, finibus leo.
+          </p>
+          <div class="mt-[1em] w-full text-right">
+            <a>More...</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
