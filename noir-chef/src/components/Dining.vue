1601cfefc83c2b99a0f534d2f7d0eede2432971c
Favour Ukonu <favouroukonu@gmail.com>
2024-01-16 22:02:23 +0100

    Chore: Dining component completed

--- noir-chef/src/components/Dining.vue
+++ noir-chef/src/components/Dining.vue
@@ -31,14 +31,14 @@
       </div>
     </div>
 
-    <div class="flex flex-col justify-center items-center gap-5">
+    <div class="flex flex-col justify-center items-center gap-[8em]">
       <!-- 1 -->
       <div
         class="flex justify-center lg:items-start lg:gap-[4em] lg:flex-row flex-col items-center"
       >
         <div class="text-textColor flex flex-col gap-[1em]">
           <h1 class="font-header text-4xl font-semibold">Fine Dining</h1>
-          <p class="font-bigText">
+          <p class="font-bigText text-base">
             Bottle of Champagne<br />
             Fine Sushi Tower For 2+<br />
             Dessert
@@ -46,7 +46,7 @@
         </div>
 
         <div
-          class="lg:w-[65%] w-[90%] flex items-end gap-2 lg:flex-row flex-col"
+          class="lg:w-[60%] w-[90%] flex items-end gap-2 lg:flex-row flex-col"
         >
           <p class="text-headerText font-header text-4xl font-semibold">$500</p>
           <img
@@ -55,30 +55,59 @@
           />
         </div>
       </div>
-    </div>
 
-    <!-- <div class="flex justify-center items-center">
-      <div class="flex justify-center items-center w-[90%] lg:w-[70%]">
+      <!-- 2 -->
+      <div
+        class="flex justify-center lg:items-start lg:gap-[4em] lg:flex-row-reverse flex-col items-center"
+      >
+        <div class="text-textColor flex flex-col gap-[1em]">
+          <h1 class="font-header text-4xl font-semibold">Gold Dining</h1>
+          <p class="font-bigText lg:text-right text-base">
+            Bottle of Champagne<br />
+            Secret Menu Sushi For 2+<br />
+            Dessert
+          </p>
+        </div>
+
         <div
-          class="flex flex-col lg:flex-row justify-end items-start gap-[8em] gradient"
+          class="lg:w-[60%] w-[90%] flex items-end gap-2 lg:flex-row-reverse flex-col"
         >
-          <div class="text-textColor">
-            <h1>Fine Dining</h1>
-            <p>
-              Bottle of Champagne<br />
-              Fine Sushi Tower For 2+<br />
-              Dessert
-            </p>
-          </div>
-
-          <div class="flex justify-center lg:justify-end items-end w-[65%]">
-            <p class="text-headerText">$500</p>
-            <img src="../assets/Rectangle 17.png" class="w-[full] h-[full]" />
-          </div>
+          <p class="text-headerText font-header text-4xl font-semibold">
+            $1000
+          </p>
+          <img
+            src="../assets/Rectangle 19.png"
+            class="w-[full] h-[full] gradient"
+          />
         </div>
       </div>
-    </div> -->
-    <h1 class="text-textColor">Dining Event</h1>
+
+      <!-- 3 -->
+      <div
+        class="flex justify-center lg:items-start lg:gap-[4em] lg:flex-row flex-col items-center"
+      >
+        <div class="text-textColor flex flex-col gap-[1em]">
+          <h1 class="font-header text-4xl font-semibold">Royalty Dining</h1>
+          <p class="font-bigText text-base">
+            Bottle of Luxury Champagne<br />
+            Fine Sushi Tower For 2+<br />
+            Dessert
+          </p>
+        </div>
+
+        <div
+          class="lg:w-[60%] w-[90%] flex items-end gap-2 lg:flex-row flex-col"
+        >
+          <p class="text-headerText font-header text-4xl font-semibold">
+            $1500
+          </p>
+          <img
+            src="../assets/Rectangle 21.png"
+            class="w-[full] h-[full] gradient"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
